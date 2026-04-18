ed363b5a8842a48311293f36d60f01d98cb636f3
samuelpariatanta <samuelpro3333@gmail.com>
2026-04-17 22:03:17 -0500

    nodo

---
+++ src/listaEnlazada/Nodo.java
@@ -0,0 +1,10 @@
+// El vagón del tren: guarda el dato y apunta al siguiente
+public class Nodo {
+    String dato;
+    Nodo siguiente;
+
+    public Nodo(String dato) {
+        this.dato = dato;
+        this.siguiente = null;
+    }
+}
