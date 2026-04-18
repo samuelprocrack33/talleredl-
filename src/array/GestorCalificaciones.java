6180cfd74dde3c4ca3000977d6de8764aba595ae
samuelpariatanta <samuelpro3333@gmail.com>
2026-04-17 20:37:13 -0500

    gestor calificaciones completo

--- src/array/GestorCalificaciones.java
+++ src/array/GestorCalificaciones.java
@@ -34,4 +34,74 @@ public class GestorCalificaciones {
         for (int i = 0; i < totalEstudiantes; i++)
             suma += calificaciones[i];
         return suma / totalEstudiantes;
-   
+    }
+
+    public double mayor() {
+        if (totalEstudiantes == 0) return 0;
+        double max = calificaciones[0];
+        for (int i = 1; i < totalEstudiantes; i++)
+            if (calificaciones[i] > max) max = calificaciones[i];
+        return max;
+    }
+
+    // E1.1
+    public double menor() {
+        if (totalEstudiantes == 0) return 0;
+        double min = calificaciones[0];
+        for (int i = 1; i < totalEstudiantes; i++)
+            if (calificaciones[i] < min) min = calificaciones[i];
+        return min;
+    }
+
+    // E1.2
+    public int buscarNota(double nota) {
+        for (int i = 0; i < totalEstudiantes; i++)
+            if (calificaciones[i] == nota) return i;
+        return -1;
+    }
+
+    // E1.3
+    public void ordenarBurbuja() {
+        for (int i = 0; i < totalEstudiantes - 1; i++)
+            for (int j = 0; j < totalEstudiantes - i - 1; j++)
+                if (calificaciones[j] > calificaciones[j + 1]) {
+                    double temp = calificaciones[j];
+                    calificaciones[j] = calificaciones[j + 1];
+                    calificaciones[j + 1] = temp;
+                }
+    }
+
+    // E1.4
+    public int contarAprobados(double minAprobatoria) {
+        int conteo = 0;
+        for (int i = 0; i < totalEstudiantes; i++)
+            if (calificaciones[i] >= minAprobatoria) conteo++;
+        return conteo;
+    }
+
+    // E1.6
+    public static double[] union(double[] a, double[] b) {
+        double[] temp = new double[a.length + b.length];
+        int cont = 0;
+        for (double v : a) {
+            boolean existe = false;
+            for (int i = 0; i < cont; i++) if (temp[i] == v) { existe = true; break; }
+            if (!existe) temp[cont++] = v;
+        }
+        for (double v : b) {
+            boolean existe = false;
+            for (int i = 0; i < cont; i++) if (temp[i] == v) { existe = true; break; }
+            if (!existe) temp[cont++] = v;
+        }
+        double[] resultado = new double[cont];
+        System.arraycopy(temp, 0, resultado, 0, cont);
+        return resultado;
+    }
+
+    public void mostrar() {
+        System.out.print("Calificaciones: ");
+        for (int i = 0; i < totalEstudiantes; i++)
+            System.out.print(calificaciones[i] + (i < totalEstudiantes - 1 ? ", " : ""));
+        System.out.println();
+    }
+}
